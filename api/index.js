5daba561fc2934f7c4f43ead01f0f1f444cb43bd
TomasTinto1234 <tititinto@gmail.com>
2022-10-27 14:07:41 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -21,8 +21,8 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(process.env.PORT, () => {
+conn.sync({alter: true, force: false }).then(() => {
+  server.listen(process.env.PORT || 3001, () => {
     console.log(`server listening at port ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
