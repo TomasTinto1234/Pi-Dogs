a17a8235a5fa016e6e6ad90ae2171f9311322dab
TomasTinto1234 <tititinto@gmail.com>
2022-12-05 16:25:05 -0300

    ml

--- src/db.js
+++ src/db.js
@@ -27,7 +27,7 @@ let sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/lookHouse`, {
+    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
         logging: false,
         native: false,
       });
